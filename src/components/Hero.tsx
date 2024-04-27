6bbcafde09674abaee642b43e66ae26ce7927f55
Syed-Ahmed02 <55893792+Syed-Ahmed02@users.noreply.github.com>
2024-04-26 23:59:22 -0400

    added handdrawn svg line

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,19 +1,15 @@
-"use client";
 import { Badge } from "@/components/ui/badge";
-import { motion } from "framer-motion";
+import { Handdrawn_underline } from "./svgs";
 export const Hero = () => (
-    <motion.div
-        initial={{ opacity: 0 }} // Start from fully transparent
-        whileInView={{ opacity: 1 }} // Animate to fully opaque when in view
-        viewport={{ once: true }}
-        transition={{ duration: 1 }} 
+    <div
         className="w-full  py-20 lg:py-40">
         <div className="container mx-auto">
             <div className="grid grid-cols-1 gap-8  lg:grid-cols-2">
                 <div className="flex gap-4 flex-col">
                     <div className="flex gap-4 flex-col">
                         <h1 className="text-5xl md:text-7xl max-w-lg tracking-tighter text-left font-bold">
-                            My Name is Syed
+                            My Name is <span className="h-10">Syed Ahmed <Handdrawn_underline /></span>
+
                         </h1>
                         <div className="text-xl leading-relaxed tracking-tight text-muted-foreground max-w-md text-left font-normal font-bodnoi">
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Donec ultrices tincidunt arcu non sodales neque sodales ut. Consequat nisl vel pretium lectus quam id leo in vitae. Tortor aliquam nulla facilisi cras fermentum odio eu.</p>
@@ -31,5 +27,5 @@ export const Hero = () => (
                 </div>
             </div>
         </div>
-    </motion.div>
+    </div>
 );
---
+++ src/components/svgs.tsx
@@ -0,0 +1,40 @@
+import { cn } from "@/lib/utils"
+
+function Handdrawn_underline({
+    className,
+    inverted,
+    ...props
+}: React.ComponentProps<'svg'> & { inverted?: boolean }) {
+    return (
+        <svg
+
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className={cn("", className)}
+            {...props}
+            viewBox="20 16 375 28">
+            <path
+                fill="none"
+                stroke="hsl(197 71% 73%)"
+                strokeDasharray="14 8"
+                strokeLinecap="round"
+                strokeWidth="6"
+                d="M24.664 35.874c2.762-1.13 14.332-7.57 19.73-8.071 5.4-.503 13.812 3.73 18.835 4.484 5.022.753 12.394.771 17.04.897 4.646.125 10.744.251 16.144 0 5.399-.251 16.77-1.292 22.421-1.794 5.65-.502 10.655-.287 17.937-1.794 7.283-1.506 25.292-8.09 34.081-8.968 8.79-.88 19.66 2.44 28.7 2.69 9.04.252 24.825-.896 35.874-.896 11.05 0 30.493.52 43.05.896 12.555.377 40.107 1.543 46.636 1.794"
+            ></path>
+            <defs>
+                <linearGradient id="SvgjsLinearGradient1001">
+                    <stop offset="0" stopColor="hsl(37, 99%, 67%)"></stop>
+                    <stop offset="1" stopColor="hsl(316, 73%, 52%)"></stop>
+                </linearGradient>
+            </defs>
+        </svg>
+    )
+}
+
+function Icon() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 800 400"><path d="M215.695068359375,204.4842987060547C227.80269368489584,203.13900756835938,262.182373046875,198.2062733968099,288.3408203125,196.4125518798828C314.499267578125,194.61883036295572,345.1420186360677,188.7892328898112,372.645751953125,193.7219696044922C400.1494852701823,198.6547063191732,419.2825012207031,223.0194346110026,453.36322021484375,226.00897216796875C487.4439392089844,228.9985097249349,556.5022583007812,214.0508244832357,577.1300659179688,211.65919494628906" fill="none" stroke-width="25" stroke="hsl(197 71% 73)" stroke-linecap="round"></path><defs><linearGradient id="SvgjsLinearGradient1001"><stop stop-color="hsl(37, 99%, 67%)" offset="0"></stop><stop stop-color="hsl(316, 73%, 52%)" offset="1"></stop></linearGradient></defs></svg>
+    );
+}
+
+export { Handdrawn_underline }
