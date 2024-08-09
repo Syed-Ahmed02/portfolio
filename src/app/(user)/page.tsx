ae6296019d2cb2ecadc5258eb93d81b41265b3d8
Syed-Ahmed02 <55893792+Syed-Ahmed02@users.noreply.github.com>
2024-08-09 07:22:12 -0400

    added s3,shadcn,tailwindcss

---
+++ src/app/(user)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div className='bg-black min-h-screen flex flex-row items-center justify-center'>
+        <Button variant="outline">Test button</Button>
+    </div>
+  )
+}
+
+export default Page
