effcbbe017c850e52af7195dc18057a59f9d26e1
Syed <syedahmedd.02@gmail.com>
2025-04-12 18:34:57 -0400

    migrations

--- migrations/index.ts
+++ migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250412_130449 from './20250412_130449';
+import * as migration_20250412_223342 from './20250412_223342';
 
 export const migrations = [
   {
     up: migration_20250412_130449.up,
     down: migration_20250412_130449.down,
-    name: '20250412_130449'
+    name: '20250412_130449',
+  },
+  {
+    up: migration_20250412_223342.up,
+    down: migration_20250412_223342.down,
+    name: '20250412_223342'
   },
 ];
